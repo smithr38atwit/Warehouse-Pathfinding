f200770c1d4f2fd190ce7ed9dccb8f5611e86afd
Ryan Smith <smithr38@wit.edu>
2023-08-09 04:00:20 -0400

    current progress

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from pydantic import BaseModel
 
 
 class State(BaseModel):
-    robot_locs: set[tuple[int, int]]
-    holding_box: tuple
+    robot_locs: list[tuple[int, int]]
+    holding_boxes: list[bool]
     box_locs: set[tuple[int, int]]
     blocked_locs: set[tuple[int, int]]
     shelf_locs: set[tuple[int, int]]
@@ -19,8 +19,8 @@ class State(BaseModel):
     def __hash__(self):
         return hash(
             (
-                self.robot_locs,
-                self.holding_box,
+                tuple(self.robot_locs),
+                tuple(self.holding_boxes),
                 frozenset(self.box_locs),
                 frozenset(self.blocked_locs),
                 frozenset(self.shelf_locs),
@@ -32,6 +32,7 @@ class Node(BaseModel):
     depth: int
     priority: float = 0
     action: str = None
+    robot: int = None
     state: State
     parent: "Node" = None
 
@@ -55,8 +56,8 @@ def main():
         COLS = int(file.readline().strip())
         ROWS = int(file.readline().strip())
 
-        robots = set()
-        n = 0
+        robots = []
+        holding = []
         boxes = set()
         shelves = set()
         blocked = set()
@@ -65,8 +66,8 @@ def main():
             x = 0
             for char in line:
                 if char == "@":  # robot
-                    robots.add((x, y))
-                    n += 1
+                    robots.append((x, y))
+                    holding.append(False)
                 elif char == "#":  # blocked cell (e.g. filled shelf)
                     blocked.add((x, y))
                 elif char == "|":  # empty shelf
@@ -78,7 +79,7 @@ def main():
 
     init_state = State(
         robot_locs=robots,
-        holding_box=(False,) * n,
+        holding_boxes=holding,
         box_locs=boxes,
         blocked_locs=blocked,
         shelf_locs=shelves,
@@ -94,7 +95,7 @@ def main():
 # ------ ALGORITHM ------
 
 
-def astar(init_state):
+def astar(init_state: State):
     init_node = Node(state=init_state, depth=0)
     open = queue.PriorityQueue()
     closed = set()
@@ -113,8 +114,8 @@ def astar(init_state):
             continue
         closed.add(node.state)
 
-        if len(node.state.box_locs) == 0 and not node.state.holding_box:
-            print_path(node)
+        if len(node.state.box_locs) == 0 and not any(node.state.holding_boxes):
+            print_path(node, len(init_state.robot_locs))
             break
         else:
             children = expand(node, closed)
@@ -135,84 +136,92 @@ def astar(init_state):
 def expand(node: Node, closed: set) -> list[Node]:
     state = copy.deepcopy(node.state)
     children = []
-    x, y = state.robot_locs
 
-    # Expand order: pickup, place, up, right, down, left
-    directions = [(x, y - 1, "U"), (x + 1, y, "R"), (x, y + 1, "D"), (x - 1, y, "L")]
+    for i, (x, y) in enumerate(state.robot_locs):
+        # Expand order: pickup, place, up, right, down, left
+        directions: list[tuple[int, int, str]] = [(x, y - 1, "U"), (x + 1, y, "R"), (x, y + 1, "D"), (x - 1, y, "L")]
 
-    # If box can be picked up, generate state with pickup action and skip movement actions
-    if state.robot_locs in state.box_locs and not state.holding_box:
-        state.box_locs.remove(state.robot_locs)
-        state.holding_box = True
-        child = Node(state=copy.deepcopy(state), depth=node.depth + 1, action="PU", parent=node)
-        children.append(child)
-        return children
-    # If box can be placed, generate state with place action and skip movement actions
-    elif state.robot_locs in state.shelf_locs and state.holding_box:
-        state.shelf_locs.remove(state.robot_locs)
-        state.blocked_locs.add(state.robot_locs)
-        state.holding_box = False
-        child = Node(state=copy.deepcopy(state), depth=node.depth + 1, action="PL", parent=node)
-        children.append(child)
-        return children
+        # If box can be picked up, generate state with pickup action and skip movement actions
+        if (x, y) in state.box_locs and not state.holding_boxes[i]:
+            state.box_locs.remove((x, y))
+            state.holding_boxes[i] = True
+            child = Node(state=copy.deepcopy(state), depth=node.depth + 1, action="PU", robot=i, parent=node)
+            children.append(child)
+            return children
+        # If box can be placed, generate state with place action and skip movement actions
+        elif (x, y) in state.shelf_locs and state.holding_boxes[i]:
+            state.shelf_locs.remove((x, y))
+            state.blocked_locs.add((x, y))
+            state.holding_boxes[i] = False
+            child = Node(state=copy.deepcopy(state), depth=node.depth + 1, action="PL", robot=i, parent=node)
+            children.append(child)
+            return children
 
-    # Expand directions
-    for loc in directions:
-        # Skip state if location is out of bounds or blocked
-        if (
-            (loc[0] < 0 or loc[0] > COLS - 1)
-            or (loc[1] < 0 or loc[1] > ROWS - 1)
-            or ((loc[0], loc[1]) in state.blocked_locs)
-        ):
-            continue
-        new_state = State(
-            robot_locs=(loc[0], loc[1]),
-            holding_box=state.holding_box,
-            box_locs=state.box_locs,
-            blocked_locs=state.blocked_locs,
-            shelf_locs=state.shelf_locs,
-        )
+        # Expand directions
+        for new_x, new_y, action in directions:
+            # Skip state if location is out of bounds or blocked
+            if (
+                (new_x < 0 or new_x > COLS - 1)
+                or (new_y < 0 or new_y > ROWS - 1)
+                or ((new_x, new_y) in state.blocked_locs)
+            ):
+                continue
 
-        if new_state in closed:
-            continue
+            new_state = State(
+                robot_locs=state.robot_locs,
+                holding_boxes=state.holding_boxes,
+                box_locs=state.box_locs,
+                blocked_locs=state.blocked_locs,
+                shelf_locs=state.shelf_locs,
+            )
+            new_state.robot_locs[i] = (x, y)
 
-        child = Node(state=new_state, depth=node.depth + 1, action=loc[2], parent=node)
-        children.append(child)
+            if new_state in closed:
+                continue
+
+            child = Node(state=new_state, depth=node.depth + 1, action=action, robot=i, parent=node)
+            children.append(child)
 
     return children
 
 
 def heuristics(state: State) -> float:
     # Manhattan distance
-    dsts = []
-    rbt = state.robot_locs
-    # When not holding box
-    if not state.holding_box:
-        for box in state.box_locs:
-            dst = abs(rbt[0] - box[0]) + abs(rbt[1] - box[1])
-            dsts.append(dst)
-        return min(dsts) if len(dsts) != 0 else 0
-    # When holding box
-    else:
-        for shelf in state.shelf_locs:
-            dst = abs(rbt[0] - shelf[0]) + abs(rbt[1] - shelf[1])
-            dsts.append(dst)
-        return min(dsts) if len(dsts) != 0 else 0
+    dsts = 0
+    for i, (x, y) in enumerate(state.robot_locs):
+        sub_dsts = []
+        # When not holding box
+        if not state.holding_boxes[i]:
+            for box in state.box_locs:
+                dst = abs(x - box[0]) + abs(y - box[1])
+                sub_dsts.append(dst)
+            dsts += min(sub_dsts) if len(sub_dsts) != 0 else 0
+        # When holding box
+        else:
+            for shelf in state.shelf_locs:
+                dst = abs(x - shelf[0]) + abs(y - shelf[1])
+                sub_dsts.append(dst)
+            dsts += min(sub_dsts) if len(sub_dsts) != 0 else 0
+    return dsts
 
 
-def print_path(node: Node):
-    actions = []
-    while True:
-        if node.action == None:
-            break
-        actions.append(node.action)
-        node = node.parent
+def print_path(node: Node, num_robots: int):
+    for i in range(num_robots):
+        print(f"Robot {i+1}")
+        actions = []
+        while True:
+            if node.action == None:  # Stop when root node is reached
+                break
 
-    if INLINE:
-        print(*actions[::-1])
-    else:
-        for action in actions[::-1]:
-            print(action)
+            if node.robot == i:  # Only add action if it belongs to current robot
+                actions.append(node.action)
+            node = node.parent
+
+        if INLINE:
+            print(*actions[::-1])
+        else:
+            for action in actions[::-1]:
+                print(action)
 
 
 if __name__ == "__main__":
